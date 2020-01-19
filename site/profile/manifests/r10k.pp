8b0793cc588824623de43a7d9c44e171d08c0d30
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-18 16:05:19 -0800

    configure r10 webhook classes

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,13 @@
+class profile::r10k {
+  class { 'r10k':
+    remote  => 'https://github.com/mvilain/control-repo',
+  }
+  class { 'r10k::webhook::config":
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class { 'r10k::webhook::config":
+    user  => 'root',
+    group => 'root',
+  }
+}
