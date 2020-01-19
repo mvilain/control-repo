c1615afb0c1a2d3a569d17aca8cbbf98624a9c83
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-18 16:55:55 -0800

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,11 +2,11 @@ class profile::r10k {
   class { 'r10k':
     remote  => 'https://github.com/mvilain/control-repo',
   }
-  class { 'r10k::webhook::config":
+  class { 'r10k::webhook::config':
     use_mcollective => false,
     enable_ssl      => false,
   }
-  class { 'r10k::webhook::config":
+  class { 'r10k::webhook::config':
     user  => 'root',
     group => 'root',
   }
