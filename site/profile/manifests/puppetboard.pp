4e48ad64d1816ae818d4ececd50f3fd2083b5d79
Michael Vilain <michael@vilain.com>
2020-01-27 11:55:42 -0800

    fixed missing modules for puppetboard

--- site/profile/manifests/puppetboard.pp
+++ site/profile/manifests/puppetboard.pp
@@ -1,16 +1,32 @@
 class profile::puppetboard {
+
+  # fix issues with modules not found
+  # https://github.com/voxpupuli/puppet-puppetboard/issues/128
+
   # this file is needed to create the virtual environment for puppetboard
   file{'/srv/puppetboard/puppetboard/requirements.txt':
     ensure => present,
   }
   -> class { 'apache': }
-  class { 'apache::mod::wsgi': }
+  -> class { 'apache::mod::wsgi': }
 
 # https://github.com/voxpupuli/puppetboard/issues/527
   class { 'puppetboard': 
     manage_git        => true,
     manage_virtualenv => true,
     revision          => 'v1.0.0', # repo tag
+  }
+  -> python::pip { 'Flask':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }
+  -> python::pip { 'Flask-WTF':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }
+  -> python::pip { 'WTForms':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }
+  -> python::pip { 'pypuppetdb':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
   }
 
   # Access Puppetboard through pboard.example.com
