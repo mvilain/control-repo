e89e1bc86da626c0b5640d39fb44b5da1d2e87bf
Michael Vilain <michael@vilain.com>
2020-01-27 23:29:33 -0800

    update with puppetboard python 3 requirements

--- site/profile/manifests/puppetboard.pp
+++ site/profile/manifests/puppetboard.pp
@@ -4,43 +4,13 @@ class profile::puppetboard {
   class { 'apache': }
   class { 'apache::mod::wsgi': }
 
-  # fix issues with modules not found
-  # https://github.com/voxpupuli/puppetboard/issues/527
-  class { 'python':
-    version    => '3.6',
-    pip        => 'present',
-    dev        => 'present',
-    virtualenv => 'present',
-#    gunicorn   => 'present',
-  }
-  -> python::pip { 'packaging' : 
-    ensure => '19.0', 
-    pkgname => 'packaging', 
-  }
-  -> python::pip { 'Flask':
-    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
-  }
-  -> python::pip { 'Flask-WTF':
-    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
-  }
-  -> python::pip { 'WTForms':
-    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
-  }
-  -> python::pip { 'pypuppetdb':
-    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
-  }
-
-  # puppetboard 1.1 requires python 3.[678]
+  # module puppetboard 1.1 requires python 3.[678]
   # fix issues with modules not found
   # https://github.com/voxpupuli/puppet-puppetboard/issues/128
   # this file is needed to create the virtual environment for puppetboard
-  file{'/srv/puppetboard/puppetboard/requirements.txt':
-    ensure => present,
-  }
-  -> class { 'puppetboard': 
+  class { 'puppetboard': 
     manage_git        => true,
     manage_virtualenv => true,
-    #revision          => 'v1.1.0', # repo tag
   }
 
   # Access Puppetboard through pboard.example.com
